61ffcdce4c14505240d821b87bb668408c0885e8
Pablo <pacupa@gmail.com>
2017-04-27 03:12:59 -0400

    update files

--- hello.pymongo.bottle.py
+++ hello.pymongo.bottle.py
@@ -5,6 +5,7 @@ import pymongo
 # this is the handler for the default path of the web server
 
 @bottle.route('/')
+
 def index():
     
     # connect to mongoDB
@@ -20,7 +21,7 @@ def index():
     # find a single document
     item = name.find_one()
 
-    return '<b>Hello %s!</b>' % item['name']
+    return '<b>Hello %s!</b>' % item['nombre']
 
 
 bottle.run(host='localhost', port=8082)
